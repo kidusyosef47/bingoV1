8f81e9fb2f1afc74ea4c2445ad5087d36ed6764b
kidusyosef47 <43100419+kidusyosef47@users.noreply.github.com>
2026-01-14 12:24:53 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ tg.expand();
 const cardsDiv = document.getElementById("cards");
 
 for (let i = 1; i <= 100; i++) {
-  const btn = document.createElement("div");
-  btn.className = "card";
-  btn.innerText = i;
-  btn.onclick = () => {
+  const card = document.createElement("div");
+  card.className = "card";
+  card.innerText = i;
+
+  card.onclick = () => {
     tg.sendData("card_" + i);
   };
-  cardsDiv.appendChild(btn);
+
+  cardsDiv.appendChild(card);
 }
